857f436462fb691a46f619464279d8cb73598e5b
Leah <scout1665@gmail.com>
2024-09-13 21:39:46 -0500

    added sh file for testing

---
+++ HW01/task4.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+
+#SBATCH -J FirstSlurm
+
+#SBATCH -o FirstSlurm.out -e FirstSlurm.err
+
+#SBATCH -t 0-00:01:00
+
+#SBATCH -c 2
+
+cd $SLURM_SUBMIT_DIR
+
+date
+hostname
