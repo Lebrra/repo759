e56f68748e05fa6596da3ed2b47703725c0d4743
Leah <scout1665@gmail.com>
2024-09-22 15:07:09 -0500

    runs task1.cpp now

--- HW02/task1.sh
+++ HW02/task1.sh
@@ -3,14 +3,19 @@
 #SBATCH -p instruction
 #SBATCH -J HW02_Task1
 #SBATCH -o Task2_1.out -e Task2_1.err
-#SBATCH -t 0-00:01:00
+#SBATCH -t 0-00:05:00
 #SBATCH --gres=gpu:1 -c 1
 
 module load gcc/13.2.0
 module load nvidia/cuda
+g++ scan.cpp task1.cpp -Wall -O3 -std=c++17 -o task1
 
 date
 
 pow=1
 for ((i=0; i<9; i++)) do pow=$(($pow*2)); done
-for ((i=0; i<10; i++)) do pow=$(($pow*2)); echo "$pow"; done
+for ((i=0; i<20; i++)) 
+do 
+    pow=$(($pow*2)); 
+    ./task1 $pow; 
+done
