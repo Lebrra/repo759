82aee5d5f71b3130daa363214326debb02fec9d3
Leah <scout1665@gmail.com>
2024-09-24 21:16:57 -0500

    more code cleanup

--- HW02/task2.cpp
+++ HW02/task2.cpp
@@ -6,120 +6,6 @@
 
 using namespace std;
 
-void tester2D() {
-	int n = 4;
-	int m = 3;
-
-	float** f;
-	f = (float**)malloc(sizeof(float*) * 4);
-	f[0] = (float*)malloc(sizeof(float) * 4);
-	f[0][0] = 1;
-	f[0][1] = 3;
-	f[0][2] = 4;
-	f[0][3] = 8;
-	f[1] = (float*)malloc(sizeof(float) * 4);
-	f[1][0] = 6;
-	f[1][1] = 5;
-	f[1][2] = 2;
-	f[1][3] = 4;
-	f[2] = (float*)malloc(sizeof(float) * 4);
-	f[2][0] = 3;
-	f[2][1] = 4;
-	f[2][2] = 6;
-	f[2][3] = 8;
-	f[3] = (float*)malloc(sizeof(float) * 4);
-	f[3][0] = 1;
-	f[3][1] = 4;
-	f[3][2] = 5;
-	f[3][3] = 2;
-
-	cout << "F:\n";
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << f[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-
-	float** w;
-	w = (float**)malloc(sizeof(float*) * 3);
-	w[0] = (float*)malloc(sizeof(float) * 3);
-	w[0][0] = 0;
-	w[0][1] = 0;
-	w[0][2] = 1;
-	w[1] = (float*)malloc(sizeof(float) * 3);
-	w[1][0] = 0;
-	w[1][1] = 1;
-	w[1][2] = 0;
-	w[2] = (float*)malloc(sizeof(float) * 3);
-	w[2][0] = 1;
-	w[2][1] = 0;
-	w[2][2] = 0;
-
-	cout << "W:\n";
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << w[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-
-	convolve c;
-	float** g = c.doConvolve(f, w, n, m);
-	
-	cout << "G:\n";
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << g[i][j] << " ";
-		}
-		cout << endl;
-	}
-
-	delete g, f, w;
-}
-
-void tester1D() {
-	int n = 4;
-	int m = 3;
-
-	float fVals[16] = { 1, 3, 4, 8, 6, 5, 2, 4, 3, 4, 6, 8, 1, 4, 5, 2 };
-	float wVals[9] = {0, 0, 1, 0, 1, 0, 1, 0, 0};
-
-	float* f;
-	//f = (float*)malloc(sizeof(float) * 4 * 4);
-	f = fVals;
-
-	cout << "F:\n";
-	for (int i = 0; i < n * n; i++) {
-		cout << f[i] << " ";
-		if (i % n == n - 1) cout << endl;
-	}
-	cout << endl;
-
-	float* w;
-	w = wVals;
-
-	cout << "W:\n";
-	for (int i = 0; i < m * m; i++) {
-		cout << w[i] << " ";
-		if (i % m == m - 1) cout << endl;
-	}
-	cout << endl;
-
-	convolve c;
-	float* g = c.doConvolve(f, w, n, m);
-	
-	cout << "G:\n";
-	for (int i = 0; i < n * n; i++) {
-		cout << g[i] << " ";
-		if (i % n == n - 1) cout << endl;
-	}
-
-	delete g, f, w;
-}
-
 int main(int argc, char* argv[])
 {
 	auto start = chrono::steady_clock::now();
@@ -170,6 +56,6 @@ int main(int argc, char* argv[])
 	cout << "last element:   \t" << g[n*n - 1] << endl;
 	
 	
-	delete randomF, randomW, g;
+	free(randomF, randomW, g);
 }
 
