f5a173ee268c72b0a6fd048de0eeb94b7f551e9d
Leah <scout1665@gmail.com>
2024-09-26 22:29:19 -0500

    task3 with provided .h

---
+++ HW02/matmul.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstddef>
+#include <vector>
+using namespace std;
+
+#ifndef MATMUL_H
+#define MATMUL_H
+
+// assuming all matrices are square
+class matmul {
+public:
+	void mmul1(const double* A, const double* B, double* C, const unsigned int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					// row = i
+					// column = j
+					// A[i, j] = i * n + j
+					// k = amount of additions to do to get value at C[i, j]
+					
+					C[i * n + j] += A[i * n + k] * B[k * n + j];
+				}
+			}
+		}
+	}
+
+	void mmul2(const double* A, const double* B, double* C, const unsigned int n) {
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k < n; k++) {
+				for (int j = 0; j < n; j++) {
+					C[i * n + j] += A[i * n + k] * B[k * n + j];
+				}
+			}
+		}
+	}
+
+	void mmul3(const double* A, const double* B, double* C, const unsigned int n) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				for (int i = 0; i < n; i++) {
+					C[i * n + j] += A[i * n + k] * B[k * n + j];
+				}
+			}
+		}
+	}
+
+	void mmul4(const vector<double>& A, const vector<double>& B, double* C, const unsigned int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					C[i * n + j] += A[i * n + k] * B[k * n + j];
+				}
+			}
+		}
+	}
+};
+
+#endif
---
+++ HW02/task3.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+#include "matmul.cpp"
+#include "matmul.h"
+
+using namespace std;
+
+void empty(double* arr, int length) {
+	if (arr) {
+		for (int i = 0; i < length; i++) {
+			arr[i] = 0;
+		}
+	}
+}
+
+void randFill(double* arr, vector<double>& vect, int length) {
+	vect.clear();
+
+	if (arr) {
+		for (int i = 0; i < length; i++) {
+			double r = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 2)) - 1;
+			arr[i] = r;
+			vect.push_back(r);
+		}
+	}
+}
+
+void printArray(double* arr, int n) {
+	if (arr) {
+		cout << "Matrix:\n";
+		for (int i = 0; i < n * n; i++) {
+			cout << arr[i] << " ";
+			if (i % n == n - 1) cout << endl;
+		}
+		cout << endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int n = 5000;
+
+	// seeding with chrono so I don't have to include the ctime library:
+	srand(chrono::system_clock::now().time_since_epoch().count());
+
+	// create data:
+	double* a = (double*)malloc(sizeof(double) * n * n);
+	double* b = (double*)malloc(sizeof(double) * n * n);
+	double* c = (double*)malloc(sizeof(double) * n * n);
+
+	vector<double> aVect;
+	vector<double> bVect;
+	
+	randFill(a, aVect, n * n);
+	randFill(b, bVect, n * n);
+
+	matmul mat;
+
+	for (int i = 0; i < 4; i++) {
+		empty(c, n * n);
+
+		auto start = chrono::steady_clock::now();
+
+		switch (i) {
+			case 0: 
+				mat.mmul1(a, b, c, n);
+				break;
+			case 1:
+				mat.mmul2(a, b, c, n);
+				break;
+			case 2: 
+				mat.mmul3(a, b, c, n);
+				break;
+			case 3:
+				mat.mmul4(aVect, bVect, c, n);
+				break;
+		}
+
+		auto end = chrono::steady_clock::now();
+		auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+		cout << "matmul.mmul" << (i + 1) << "()" << endl;
+		cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
+		if (c) cout << "last element:\t\t" << c[n * n - 1] << endl;
+		cout << endl;
+		//printArray(c, n);
+	}
+
+	free(a);
+	free(b);
+	free(c);
+
+	aVect = vector<double>();
+	bVect = vector<double>();
+}
