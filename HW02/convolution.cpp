c83e51ba5b7d563e25b7fd947b3dd689fe8f7d29
Leah <scout1665@gmail.com>
2024-09-29 13:32:31 -0500

    fixed task2 with source code

---
+++ HW02/convolution.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+#ifndef CONVOLVE_H
+
+// I added this function because it made more sense to me; when I throw this all in a single method I don't get the same answers :(
+float getValueAtXY(int x, int y, const float* f, const float* w, size_t n, size_t m) {
+	// m is assumed to be positive
+	int mHalf = (m - 1) / 2;
+	float sum = 0;
+
+	for (int all = 0; all < m * m; all++) {
+		int i = all / m;
+		int j = all % m;
+
+		float wVal = w[all];
+		float fVal;
+
+		int fi = x + i - mHalf;
+		int fj = y + j - mHalf;
+
+		// if fi and fj are out of bounds:
+		if ((fi < 0 || fi >= n) && (fj < 0 || fj >= n)) {
+			fVal = 0;
+		}
+		// if fi OR fj are out of bounds:
+		else if (fi < 0 || fi >= n || fj < 0 || fj >= n) {
+			fVal = 1;
+		}
+		// fi and fj need to be mapped to n instead of m (all):
+		else {
+			fVal = f[fi * n + fj];
+		}
+
+		sum += (wVal * fVal);
+	}
+
+	return sum;
+}
+
+void convolve(const float* image, float* output, size_t n, const float* mask, size_t m) {
+	if (output) {
+		for (int i = 0; i < n * n; i++) {		// f = [n x n] w = [m x m]
+			int x = i / n;
+			int y = i % n;
+
+			output[i] = getValueAtXY(x, y, image, mask, n, m);
+		}
+	}
+}
+
+#endif
--- HW02/task2.cpp
+++ HW02/task2.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <chrono>
 
-#include "convolve.cpp"
-#include "convolve.h"
+#include "convolution.h"
 
 using namespace std;
 
@@ -29,35 +28,39 @@ int main(int argc, char* argv[])
 
 
 	float* randomF;
-	randomF = (float*)malloc(sizeof(float) * n*n);
+	randomF = (float*)malloc(sizeof(float) * n * n);
 	if (randomF) {	// added null check here to remove warning C6011
-		for (int i = 0; i < n*n; i++) {
+		for (int i = 0; i < n * n; i++) {
 			randomF[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 20)) - 10;
 		}
 	}
 
 	float* randomW;
-	randomW = (float*)malloc(sizeof(float) * m*m);
+	randomW = (float*)malloc(sizeof(float) * m * m);
 	if (randomW) {	// added null check here to remove warning C6011
-		for (int i = 0; i < m*m; i++) {
+		for (int i = 0; i < m * m; i++) {
 			randomW[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 2)) - 1;
 		}
 	}
 
-	convolve c;
-	float* g = c.doConvolve(randomF, randomW, n, m);
+	float* g;
+	g = (float*)malloc(sizeof(float) * n * n);
 
-	auto end = chrono::steady_clock::now();
-	auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
+	if (g) {
+		convolve(randomF, g, n, randomW, m);
 
-	cout << "Results:\n";
-	cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
-	cout << "first element:  \t" << g[0] << endl;
-	cout << "last element:   \t" << g[n*n - 1] << endl;
-	
-	
+		auto end = chrono::steady_clock::now();
+		auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+		cout << "Results:\n";
+		cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
+		cout << "first element:  \t" << g[0] << endl;
+		cout << "last element:   \t" << g[n * n - 1] << endl;
+
+		free(g);
+	}
+
 	free(randomF);
 	free(randomW);
-	free(g);
 }
 
