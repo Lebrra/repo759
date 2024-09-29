81e7ec37f7471384db4d5b55ce1d72b5a03267bb
Leah <scout1665@gmail.com>
2024-09-29 13:12:25 -0500

    fixed results again

--- HW02/task1.cpp
+++ HW02/task1.cpp
@@ -38,9 +38,10 @@ int main(int argc, char* argv[])
 		auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
 
 		cout << "Results:\n";
+		cout << "element count:  \t" << n << endl;
 		cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
 		cout << "first element:  \t" << scanned[0] << endl;
-		cout << "last element:   \t" << scanned[n - 1] << endl;
+		cout << "last element:   \t" << scanned[n - 1] << endl << endl;
 
 		free(scanned);
 	}
