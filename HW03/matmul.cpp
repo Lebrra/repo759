39b2d8b4267cfed7ae0bf5d8fd4ad05d8252a422
Leah <scout1665@gmail.com>
2024-10-14 18:30:54 -0500

    next attempt

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -5,8 +5,9 @@ using namespace std;
 #ifndef MATMUL_H
 
 void mmul(const float* A, const float* B, float* C, size_t n) {
-//#pragma omp parallel for collapse(3)
+#pragma omp parallel for
 	for (int i = 0; i < n; i++) {
+		#pragma opm parallel for
 		for (int k = 0; k < n; k++) {
 			#pragma omp parallel for
 			for (int j = 0; j < n; j++) {
