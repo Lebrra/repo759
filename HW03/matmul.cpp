1cbf0ece66de982daa98ffca44c4046406045e45
Leah <scout1665@gmail.com>
2024-10-14 18:27:29 -0500

    removed parallelization on two of the for loops

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -5,9 +5,10 @@ using namespace std;
 #ifndef MATMUL_H
 
 void mmul(const float* A, const float* B, float* C, size_t n) {
-#pragma omp parallel for collapse(3)
+//#pragma omp parallel for collapse(3)
 	for (int i = 0; i < n; i++) {
 		for (int k = 0; k < n; k++) {
+			#pragma omp parallel for
 			for (int j = 0; j < n; j++) {
 				C[i * n + j] += A[i * n + k] * B[k * n + j];
 			}
