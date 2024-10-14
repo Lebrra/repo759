872a0f4e0791f1dbb47f7c796a03ecd1834fbfb4
Leah <scout1665@gmail.com>
2024-10-14 18:22:54 -0500

    minor batch edits

--- HW03/task1_all.sh
+++ HW03/task1_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 #SBATCH -p instruction
-#SBATCH -J HW03_Task1
+#SBATCH -J HW03_Task1_ALL
 #SBATCH -o Task3_1.out -e Task3_1.err
-#SBATCH -t 0-00:01:00
+#SBATCH -t 0-00:08:00
 #SBATCH -c 20
 
 g++ task1.cpp matmul.cpp -Wall -O3 -std=c++17 -o task1 -fopenmp
