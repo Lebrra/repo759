b310095702ab1da06f071c010ed01aab9a1c0406
Leah <scout1665@gmail.com>
2024-10-14 18:41:57 -0500

    added threading for entire main instead of just mmul

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -5,11 +5,9 @@ using namespace std;
 #ifndef MATMUL_H
 
 void mmul(const float* A, const float* B, float* C, size_t n) {
-#pragma omp parallel for
+#pragma omp parallel for collapse(3)
 	for (int i = 0; i < n; i++) {
-		#pragma opm parallel for
 		for (int k = 0; k < n; k++) {
-			#pragma omp parallel for
 			for (int j = 0; j < n; j++) {
 				C[i * n + j] += A[i * n + k] * B[k * n + j];
 			}
