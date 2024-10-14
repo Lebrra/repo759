3df6467d190f9cad4f84f9244275ad10a4fe2693
Leah <scout1665@gmail.com>
2024-10-14 18:10:21 -0500

    testing with thread printout

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -1,16 +1,23 @@
 #include <cstddef>
+#include <iostream>
+#include "omp.h"
 using namespace std;
 
 #ifndef MATMUL_H
 
 void mmul(const float* A, const float* B, float* C, size_t n) {
-	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < n; k++) {
-			for (int j = 0; j < n; j++) {
-				C[i * n + j] += A[i * n + k] * B[k * n + j];
+#pragma omp parallel for collapse(3)
+	{
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k < n; k++) {
+				for (int j = 0; j < n; j++) {
+					C[i * n + j] += A[i * n + k] * B[k * n + j];
+					printf("i = %d, k = %d, j = %d, threadId = %d \n", i, k, j, omp_get_thread_num());
+				}
 			}
 		}
 	}
+
 }
 
 #endif
