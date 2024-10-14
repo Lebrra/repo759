a3f061360d6e8408bb4a93c14df6a946b513291a
Leah <scout1665@gmail.com>
2024-10-14 18:46:06 -0500

    back to split out omp fors

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -5,9 +5,12 @@ using namespace std;
 #ifndef MATMUL_H
 
 void mmul(const float* A, const float* B, float* C, size_t n) {
-#pragma omp parallel for collapse(3)
+//#pragma omp parallel for collapse(3)
+#pragma omp parallel for
 	for (int i = 0; i < n; i++) {
+		#pragma omp parallel for
 		for (int k = 0; k < n; k++) {
+			#pragma omp parallel for
 			for (int j = 0; j < n; j++) {
 				C[i * n + j] += A[i * n + k] * B[k * n + j];
 			}
