cb7d66a2d0b964b21ddca4be284f1d9e11dbe58e
Leah <scout1665@gmail.com>
2024-10-14 17:48:05 -0500

    reverted

--- HW03/task1.sh
+++ HW03/task1.sh
@@ -9,9 +9,7 @@
 g++ task1.cpp matmul.cpp -Wall -O3 -std=c++17 -o task1 -fopenmp
 date
 
-if not $2;
-then for ((t=1; t<=20; t++)) do ./task1 $1 $t; done
-elif (($1 > 1 && $2 > 0 && $2 <= 20));
+if (($1 > 1 && $2 > 0 && $2 <= 20));
 then ./task1 $1 $2;
 else echo "Invalid n or t value!"; 
 fi
