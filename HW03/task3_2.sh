b9481f7d338a9c5b148220cbe3346d5dcbd214b7
Leah <scout1665@gmail.com>
2024-10-15 21:49:50 -0500

    forgot ts was also an exponent

--- HW03/task3_2.sh
+++ HW03/task3_2.sh
@@ -10,10 +10,12 @@ g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 date
 
 ten=1
+two=1
 for ((i=0; i<6; i++)) do ten=$(($ten*10)); done
+for ((i=0; i<7; i++)) do two=$(($two*2)); done
 
 for ((t=1; t<=20; t++)) 
     do 
-        ./task3 $ten $t 9; 
+        ./task3 $ten $t $two; 
         echo "------------------------";
     done
