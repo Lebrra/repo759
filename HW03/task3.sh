291f7fd5129aa052cb3d2f3b2919a424f3d441c7
Leah <scout1665@gmail.com>
2024-10-15 21:39:19 -0500

    trying again

--- HW03/task3.sh
+++ HW03/task3.sh
@@ -3,14 +3,14 @@
 #SBATCH -p instruction
 #SBATCH -J HW03_Task3
 #SBATCH -o Task3_3.out -e Task3_3.err
-#SBATCH -t 0-00:05:00
+#SBATCH -t 0-00:10:00
 #SBATCH -c 20
 
 g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 date
 
 ten=1
-for ((i=0; i<3; i++)) do ten=$(($ten*10)); done
+for ((i=0; i<6; i++)) do ten=$(($ten*10)); done
 two=1
 
 for ((i=1; i<=10; i++)) 
