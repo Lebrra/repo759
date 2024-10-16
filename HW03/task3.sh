a55089c039b2687d8826737d14fb82ee07f2b3aa
Leah <scout1665@gmail.com>
2024-10-15 21:30:12 -0500

    testing out a smaller n

--- HW03/task3.sh
+++ HW03/task3.sh
@@ -10,11 +10,24 @@ g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 date
 
 ten=1
+for ((i=0; i<3; i++)) do ten=$(($ten*10)); done
 two=1
-for ((i=0; i<6; i++)) do ten=$(($ten*10)); done
-for ((i=1; i<=10; i++)) 
-do 
-    two=$(($two*2));
-    ./task3 $ten 8 $two; 
-    echo "------------------------";
-done
+
+if ($1 == 1) then
+    for ((i=1; i<=10; i++)) 
+    do 
+        two=$(($two*2));
+        ./task3 $ten 8 $two; 
+        echo "------------------------";
+    done
+else then
+    ten=1
+    two=1
+    for ((i=0; i<6; i++)) do ten=$(($ten*10)); done
+    for ((i=1; i<=20; i++)) 
+    do 
+        two=$(($two*2));
+        ./task3 $ten $i 10; 
+        echo "------------------------";
+    done
+fi
