8df6e075011c04062f09484997711e06ad4b8b3c
Leah <scout1665@gmail.com>
2024-10-15 21:44:56 -0500

    added second task for task3

---
+++ HW03/task3_2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+#SBATCH -J HW03_Task3
+#SBATCH -o Task3_3.out -e Task3_3.err
+#SBATCH -t 0-00:10:00
+#SBATCH -c 20
+
+g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
+date
+
+ten=1
+for ((i=0; i<6; i++)) do ten=$(($ten*10)); done
+
+for ((t=1; t<=20; t++)) 
+    do 
+        ./task3 $ten $t 9; 
+        echo "------------------------";
+    done
