f5565459c60f59236c8f837005ead607e9d4b646
Leah <scout1665@gmail.com>
2024-10-22 18:13:14 -0500

    hw04-2

---
+++ HW04/task2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+#SBATCH -J HW04_Task2
+#SBATCH -o Task4_2.out -e Task4_2.err
+#SBATCH -t 0-00:01:00
+#SBATCH -c 8
+
+g++ task2.cpp -Wall -O3 -std=c++17 -o task2
+date
+
+./task2 100 10
