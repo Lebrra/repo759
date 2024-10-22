0b34d0452f84e898fec6e479fb8c2aa63d8b31b5
Leah <scout1665@gmail.com>
2024-10-22 18:54:23 -0500

    confirming correctness

--- HW04/task2.cpp
+++ HW04/task2.cpp
@@ -20,11 +20,11 @@ const double board_size = 4.0; // Size of the board
 void getAcc(const double pos[][3], const double mass[], double acc[][3], int N) {
 
     // first reset all acc values to 0:
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 3; j++) {
-            acc[i][j] = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                acc[i][j] = 0;
+            }
         }
-    }
 
     // now generate new acc values:
     for (int i = 0; i < N; i++) {
@@ -84,110 +84,87 @@ int main(int argc, char* argv[]) {
 
     // File to save positions
     std::string filename = "positions.csv";
-
     // Clear the file before starting simulation (optional)
     std::ofstream file;
     file.open(filename, std::ofstream::out | std::ofstream::trunc);
     file.close();
-
     // Allocate dynamic arrays based on N
     double* mass = new double[N];
     double(*pos)[3] = new double[N][3];
     double(*vel)[3] = new double[N][3];
     double(*acc)[3] = new double[N][3];
-
     // Create a random number engine
     std::mt19937 generator(std::random_device{}());
-
     // Create random distributions
     std::uniform_real_distribution<double> uniform_dist(0.0, 1.0);
     std::normal_distribution<double> normal_dist(0.0, 1.0);
-
     // Simulation parameters
     double t = 0.0;
-
     // Set initial masses and random positions/velocities
-    for (int i = 0; i < N; i++) {
-        mass[i] = uniform_dist(generator);
-
-        pos[i][0] = normal_dist(generator);
-        pos[i][1] = normal_dist(generator);
-        pos[i][2] = normal_dist(generator);
-
-        vel[i][0] = normal_dist(generator);
-        vel[i][1] = normal_dist(generator);
-        vel[i][2] = normal_dist(generator);
-    }
-
+        for (int i = 0; i < N; i++) {
+            mass[i] = uniform_dist(generator);
+            for (int j = 0; j < 3; j++) {
+                pos[i][j] = normal_dist(generator);
+                vel[i][j] = normal_dist(generator);
+            }
+        }
     // Convert to Center-of-Mass frame
     double velCM[3] = { 0.0, 0.0, 0.0 };
     double totalMass = 0.0;
-    for (int i = 0; i < N; i++) {
-        velCM[0] += vel[i][0] * mass[i];
-        velCM[1] += vel[i][1] * mass[i];
-        velCM[2] += vel[i][2] * mass[i];
-        totalMass += mass[i];
-    }
-
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                velCM[0] += vel[i][j] * mass[i];
+            }
+            totalMass += mass[i];
+        }
     velCM[0] /= totalMass;
     velCM[1] /= totalMass;
     velCM[2] /= totalMass;
-
-    for (int i = 0; i < N; i++) {
-        vel[i][0] -= velCM[0];
-        vel[i][1] -= velCM[1];
-        vel[i][2] -= velCM[2];
-    }
-
+        for (int i = 0; i < N; i++) {
+            vel[i][0] -= velCM[0];
+            vel[i][1] -= velCM[1];
+            vel[i][2] -= velCM[2];
+        }
     // Initial accelerations
     getAcc(pos, mass, acc, N);
-
     // Number of timesteps
     int Nt = int(tEnd / dt);
-
     // Main simulation loop
     for (int step = 0; step < Nt; step++) {
-
         // 1/2 kick
-        for (int i = 0; i < N; i++) {
-            vel[i][0] += acc[i][0] * dt / 2.0;
-            vel[i][1] += acc[i][1] * dt / 2.0;
-            vel[i][2] += acc[i][2] * dt / 2.0;
-        }
-
-        // Drift
-        for (int i = 0; i < N; i++) {
-            pos[i][0] += vel[i][0] * dt;
-            pos[i][1] += vel[i][1] * dt;
-            pos[i][2] += vel[i][2] * dt;
-        }
-
-
-        // Ensure particles stay within the board limits
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (pos[i][j] > board_size) pos[i][j] = board_size;
-                else if (pos[i][j] < -board_size) pos[i][j] = -board_size;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < 3; j++) {
+                    vel[i][j] += acc[i][j] * dt / 2.0;
+                }
             }
-        }
-
+        // Drift 
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < 3; j++) {
+                    pos[i][j] += vel[i][j] * dt;
+                }
+            }
+        // Ensure particles stay within the board limits
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (pos[i][j] > board_size) pos[i][j] = board_size;
+                    else if (pos[i][j] < -board_size) pos[i][j] = -board_size;
+                }
+            }
+        
         // Update accelerations
         getAcc(pos, mass, acc, N);
-
         // (1/2) kick
-        for (int i = 0; i < N; i++) {
-            vel[i][0] += acc[i][0] * dt / 2.0;
-            vel[i][1] += acc[i][1] * dt / 2.0;
-            vel[i][2] += acc[i][2] * dt / 2.0;
-        }
-
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < 3; j++) {
+                    vel[i][j] += acc[i][j] * dt / 2.0;
+                }
+            }
+        
         // Update time
         t += dt;
-
         // For debug: save positions to CSV at each step
-        //savePositionsToCSV(pos, N, step, filename);
+        savePositionsToCSV(pos, N, step, filename);
     }
-
     // Clean up dynamically allocated memory
     delete[] mass;
     delete[] pos;
