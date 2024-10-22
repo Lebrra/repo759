50010db943f1421b2aca5c3beccb35a6c6aa1372
Leah <scout1665@gmail.com>
2024-10-22 18:31:12 -0500

    task3

---
+++ HW04/task3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+#SBATCH -J HW04_Task3
+#SBATCH -o Task4_3.out -e Task4_3.err
+#SBATCH -t 0-00:01:00
+#SBATCH -c 8
+
+g++ task3.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
+date
+
+./task2 100 10 4
