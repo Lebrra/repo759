de4f5c9e4c11cb2bbdf74d074f4ad5f38e630096
Leah <scout1665@gmail.com>
2024-10-22 20:35:03 -0500

    changed N

--- HW04/task4_dynamic.sh
+++ HW04/task4_dynamic.sh
@@ -3,7 +3,7 @@
 #SBATCH -p instruction
 #SBATCH -J HW04_Task4
 #SBATCH -o Task4_4.out -e Task4_4.err
-#SBATCH -t 0-00:01:00
+#SBATCH -t 0-00:08:00
 #SBATCH -c 8
 
 g++ task4_dynamic.cpp -Wall -O3 -std=c++17 -o task4 -fopenmp
@@ -11,5 +11,5 @@ date
 
 for ((t=1; t<=8; t++)) 
 do
-./task4 500 100 $t
+./task4 300 100 $t
 done
