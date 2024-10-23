f642396088795ec8331bbda106a0b5947c27b5d5
Leah <scout1665@gmail.com>
2024-10-22 20:11:12 -0500

    trying out task4

---
+++ HW04/task4_guided.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+#SBATCH -J HW04_Task4
+#SBATCH -o Task4_4.out -e Task4_4.err
+#SBATCH -t 0-00:01:00
+#SBATCH -c 8
+
+g++ task4_guided.cpp -Wall -O3 -std=c++17 -o task4 -fopenmp
+date
+
+for ((t=1; t<=8; t++)) 
+do
+./task4 100 10 $t
+done
