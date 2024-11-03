d30c720f1e2bdee68008e6505a70cc07c979b1d5
Leah <scout1665@gmail.com>
2024-11-03 16:22:32 -0600

    starting hw5

---
+++ HW05/task1.cu
@@ -0,0 +1,19 @@
+#include <cuda.h>
+#include <stdio.h>
+using namespace std;
+
+__global__ void factorialKernel(int a) { 
+    int b = 1;
+    for (int i = a; i > 1; i++){
+        b *= i;
+    }
+    printf("%d! = %d", a, b);
+}
+
+int main() {
+    for (int i = 1; i <= 8; i++){
+        factorialKernel<<<1, 8>>>(i);
+    }
+    cudaDeviceSynchronize();
+    return 0;
+}
