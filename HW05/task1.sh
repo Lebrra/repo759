e2fb7a52f664ca7d059dc1f114c143772f608ad0
Leah <scout1665@gmail.com>
2024-11-03 16:30:05 -0600

    attempt 2

--- HW05/task1.sh
+++ HW05/task1.sh
@@ -4,9 +4,10 @@
 #SBATCH -J HW05_Task1
 #SBATCH -o Task5_1.out -e Task5_1.err
 #SBATCH -t 0-00:01:00
-#SBATCH --gres=gpu:1 -c 1
+#SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 
+module load nvidia/cuda
 g++ nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
 date
 
