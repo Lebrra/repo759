b15314dd7fc4fbc0fa5107dfee9ed3cc444192b2
Leah <scout1665@gmail.com>
2024-11-04 19:21:16 -0600

    removed version

--- HW05/task1.sh
+++ HW05/task1.sh
@@ -3,12 +3,12 @@
 #SBATCH -p instruction
 #SBATCH -J HW05_Task1
 #SBATCH -o Task5_1.out -e Task5_1.err
-#SBATCH -t 0-00:01:00
+#SBATCH -t 0-00:05:00
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 
-module load nvidia/cuda/11.8.0
-module load gcc/11.3.0
+module load nvidia/cuda
+module load gcc
 nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
 date
 
