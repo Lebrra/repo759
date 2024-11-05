9687f9a91ff53dc668eea1e28a03fa8dfa5c0842
Leah <scout1665@gmail.com>
2024-11-05 16:55:58 -0600

    more debug

--- HW05/vscale.cu
+++ HW05/vscale.cu
@@ -4,7 +4,7 @@
 #ifndef VSCALE_CUH
 __global__ void vscale(const float *a, float *b, unsigned int n){
     int index = threadIdx.x + blockIdx.x * 512;
-    //printf("index = %d | n = %d\n", index, n);
+    printf("index = %d | n = %d\n", index, n);
     if (index < n) {
         if (index < 5) printf("a = %f | b = %f | a*b = %f \n", a[index], b[index], a[index] * b[index]);
         b[index] *= a[index];
