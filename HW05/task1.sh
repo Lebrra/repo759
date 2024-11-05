226873632e74d336ea6ea8efb1fd9bd8d9a0b49f
Leah <scout1665@gmail.com>
2024-11-04 19:09:53 -0600

    trying hardcoded versions

--- HW05/task1.sh
+++ HW05/task1.sh
@@ -7,7 +7,8 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 
-module load nvidia/cuda
+module load nvidia/cuda/11.8.0
+module load gcc/11.3.0
 nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
 date
 
