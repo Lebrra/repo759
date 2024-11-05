eb2f19bd34f3aedff8f072542793864657024f03
Leah <scout1665@gmail.com>
2024-11-05 17:01:41 -0600

    next copy attempt

--- HW05/task3.cu
+++ HW05/task3.cu
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     int n = atoi(argv[1]);
-    float hB[n], *dB, *dA;
+    float hB[n], hA[n], *dB, *dA;
 
     // prepping threads and blocks:
     int t = 512;
@@ -17,17 +17,20 @@ int main(int argc, char* argv[]) {
     srand(chrono::system_clock::now().time_since_epoch().count());
 
     // array initialization:
-    dA = (float*)malloc(sizeof(float)*n);
-    dB = (float*)malloc(sizeof(float)*n);
 
     for (int i = 0; i < n; i++){
-        dA[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 20)) - 10;
-        dB[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 2)) - 1;
+        hA[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 20)) - 10;
+        hB[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 2)) - 1;
 
         if (i < 5){
-            cout << " dA = " << dA[i] << " | dB = " << dB[i] << endl;
+            cout << " dA = " << hA[i] << " | dB = " << hB[i] << endl;
         }
     }
+
+    cudaMalloc((void**)&dA, sizeof(float) * n);
+    cudaMalloc((void**)&dB, sizeof(float) * n);
+    cudaMemcpy(dA, &hA, sizeof(float) * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, &hB, sizeof(float) * n, cudaMemcpyHostToDevice);
 
     // do math:
     vscale<<<b, t>>>(dA, dB, n);
