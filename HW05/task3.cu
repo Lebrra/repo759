86d63bf37e95f806643b745ac025e02885df777f
Leah <scout1665@gmail.com>
2024-11-05 16:46:14 -0600

    testing a different init

--- HW05/task3.cu
+++ HW05/task3.cu
@@ -4,27 +4,16 @@
 #include "vscale.cuh"
 using namespace std;
 
-__global__ void arrayInit(float *a, int n, float min, float max){
+__global__ void arrayInit(float *a, int n){
     int index = threadIdx.x + blockIdx.x * 512;
     if (index < n) {
-        //random_device entropy_source;
-        mt19937 generator(index);
-        uniform_real_distribution<float> dist(min, max);
-
-        if (index == 0){
-            for(int i = 0; i < 10; i++){
-                float rand = dist(generator);
-                printf("%d = %f\n", i, rand);
-            }
-        }
-
         a[index] = dist(generator);
     }
 }
 
 int main(int argc, char* argv[]) {
     int n = atoi(argv[1]);
-    float hB[n], *dB, *dA;
+    float hB[n], hA[n], *dB, *dA;
 
     // prepping threads and blocks:
     int t = 512;
@@ -32,19 +21,28 @@ int main(int argc, char* argv[]) {
     printf("threads = %d | blocks = %d\n", t, b);
 
     // randomization:
-    //random_device entropy_source;
-    //mt19937 generator(entropy_source());
-    //uniform_real_distribution<float> distA(0., 20.);
-    //uniform_real_distribution<float> distB(0., 1.);
+    random_device entropy_source;
+    mt19937 generator(entropy_source());
+    uniform_real_distribution<float> distA(0., 20.);
+    uniform_real_distribution<float> distB(0., 1.);
 
     // array initialization:
-    cudaMalloc((void**)&dA, sizeof(float) * n);
-    cudaMemset(dA, 0, n * sizeof(float));
-    cudaMalloc((void**)&dB, sizeof(float) * n);
-    cudaMemset(dB, 0, n * sizeof(float));
-    arrayInit<<<b, t>>>(dA, n, 0., 20.);
-    arrayInit<<<b, t>>>(dB, n, 0., 1.);
-    cudaDeviceSynchronize();
+    dA = (float*)malloc(sizeof(float)*n);
+    dB = (float*)malloc(sizeof(float)*n);
+
+    for (int i = 0; i < n; i++){
+        dA[i] = distA(generator);
+        dB[i] = distB(generator);
+    }
+
+    // cuda initialization:
+    //cudaMalloc((void**)&dA, sizeof(float) * n);
+    //cudaMemset(dA, 0, n * sizeof(float));
+    //cudaMalloc((void**)&dB, sizeof(float) * n);
+    //cudaMemset(dB, 0, n * sizeof(float));
+    //arrayInit<<<b, t>>>(dA, n, 0., 20.);
+    //arrayInit<<<b, t>>>(dB, n, 0., 1.);
+    //cudaDeviceSynchronize();
 
     // do math:
     vscale<<<b, t>>>(dA, dB, n);
