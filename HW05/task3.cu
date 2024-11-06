d0da16c8bcb93c3c71c71f0ffc09ec5fc26061a1
Leah <scout1665@gmail.com>
2024-11-05 18:14:31 -0600

    finishing touches

--- HW05/task3.cu
+++ HW05/task3.cu
@@ -8,10 +8,11 @@ int main(int argc, char* argv[]) {
     int n = atoi(argv[1]);
     float hB[n], hA[n], *dB, *dA;
 
+    auto start = chrono::steady_clock::now();
+
     // prepping threads and blocks:
     int t = 512;
     int b = (n + t - 1) / t;
-    printf("threads = %d | blocks = %d\n", t, b);
 
     // randomization:
     srand(chrono::system_clock::now().time_since_epoch().count());
@@ -21,10 +22,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < n; i++){
         hA[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 20)) - 10;
         hB[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 2)) - 1;
-
-        if (i < 5){
-            cout << " dA = " << hA[i] << " | dB = " << hB[i] << endl;
-        }
     }
 
     cudaMalloc((void**)&dA, sizeof(float) * n);
@@ -39,10 +36,15 @@ int main(int argc, char* argv[]) {
     // results:
     cudaMemcpy(&hB, dB, sizeof(float) * n, cudaMemcpyDeviceToHost);
 
+    auto end = chrono::steady_clock::now();
+	auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
     cout << "Results: " << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << " hB = " << hB[i] << endl;
-    }
+    cout << "element count:  \t" << n << endl;
+    cout << "blocks:         \t" << b << endl;
+	cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
+	cout << "first element:  \t" << hB[0] << endl;
+	cout << "last element:   \t" << hB[n - 1] << endl << endl;
     cout << endl;
 
     cudaFree(dB);
