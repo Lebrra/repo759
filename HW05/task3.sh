3cd8aff53cf767cdc6ddaa4144114c5cd126a002
Leah <scout1665@gmail.com>
2024-11-05 18:18:01 -0600

    full test

--- HW05/task3.sh
+++ HW05/task3.sh
@@ -12,4 +12,10 @@ module load gcc/11.3.0
 nvcc task3.cu vscale.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task3
 date
 
-./task3 50; 
+pow=1
+for ((i=0; i<9; i++)) do pow=$(($pow*2)); done
+for ((i=0; i<=20; i++)) 
+do 
+    pow=$(($pow*2)); 
+    ./task3 $pow; 
+done
