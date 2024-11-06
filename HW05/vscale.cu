e6be4a19971c0c335ef22aba3deab5235bc2a7b4
Leah <scout1665@gmail.com>
2024-11-05 18:24:04 -0600

    16 threads

--- HW05/vscale.cu
+++ HW05/vscale.cu
@@ -3,7 +3,7 @@
 
 #ifndef VSCALE_CUH
 __global__ void vscale(const float *a, float *b, unsigned int n){
-    int index = threadIdx.x + blockIdx.x * 512;
+    int index = threadIdx.x + blockIdx.x * 16;
     if (index < n) {
         b[index] *= a[index];
     }
