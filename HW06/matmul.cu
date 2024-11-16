746cefc42cd69c666ae590773d1d7e5da8016671
Leah <scout1665@gmail.com>
2024-11-15 21:08:54 -0600

    testing

--- HW06/matmul.cu
+++ HW06/matmul.cu
@@ -18,11 +18,15 @@ __global__ void matmul_kernel(const float* A, const float* B, float* C, size_t n
 
     int iIndex = threadIdx.x + blockIdx.x * n;
     if (iIndex >= n*n) return;
+    printf("Updating index %i\n", iIndex);
 
     for (int k = 0; k < n; k++){
-        int jIndex = blockIdx.x * n + k + (iIndex / n);
-        int kIndex = k * n + blockIdx.x + (iIndex % n);
-        C[iIndex] += A[jIndex] * B[kIndex];
+        int jIndex = blockIdx.x * n + k + (iIndex / n);    // at iIndex = 4 -> 4, 5, 6, 7
+        int kIndex = k * n + blockIdx.x + (iIndex % n);    // at iIndex = 1 -> 1, 5, 9, 13
+
+        int adder = A[jIndex] * B[kIndex];
+        //printf("Updating index %i\nj = %i, k = %i, adder = %d\n", iIndex, jIndex, kIndex, adder);
+        C[iIndex] += adder;
     }
 }
 
