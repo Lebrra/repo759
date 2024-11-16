9d2ad842d0c99113ff7e2f8a834f46f5f6525633
Leah <scout1665@gmail.com>
2024-11-15 21:06:47 -0600

    ready for exponents

--- HW06/task1.sh
+++ HW06/task1.sh
@@ -12,4 +12,10 @@ module load gcc/11.3.0
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
 date
 
-./task1 4 1024
+pow=1
+for ((i=0; i<4; i++)) do pow=$(($pow*2)); done
+for ((i=0; i<10; i++)) 
+do 
+    pow=$(($pow*2)); 
+    ./task1 $pow 1024; 
+done
