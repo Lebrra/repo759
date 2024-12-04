eeba19304de2da484d8d7493ab0213ba9b9752a4
Leah <scout1665@gmail.com>
2024-12-04 16:15:36 -0600

    fixing sh file

--- FINAL/OpenMP/openmp.sh
+++ FINAL/OpenMP/openmp.sh
@@ -4,12 +4,11 @@
 #SBATCH -J FINAL_Task2
 #SBATCH -o FINAL_2.out -e FINAL_2.err
 #SBATCH -t 0-00:10:00
-#SBATCH --ntasks=1
-#SBATCH --gpus-per-task=1
+#SBATCH -c 20
 
 module load nvidia/cuda/11.8.0
 module load gcc/11.3.0
-nvcc rasterize.cpp filehandler.cpp pixel.cpp -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o final2
+g++ rasterize.cpp filehandler.cpp pixel.cpp -Wall -O3 -std=c++17 -o final2 -fopenmp
 date
 
 ./final2 $1
