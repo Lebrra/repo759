7b477c499bb63bc4cd9b0aa282af64240137312f
Leah <scout1665@gmail.com>
2024-12-05 14:00:51 -0600

    less generics

--- HW07/task1.cu
+++ HW07/task1.cu
@@ -4,50 +4,115 @@
 #include "matmul.cuh"
 using namespace std;
 
-template <typename T>
-void doMatmul(int n, int blockSize, int type){
-    T hB[n*n], hA[n*n], hC[n*n], *dB, *dA, *dC;
+void doInt(int n, int blockSize){
+    int hB[n*n], hA[n*n], hC[n*n], *dB, *dA, *dC;
 
     // randomization:
     srand(chrono::system_clock::now().time_since_epoch().count());
 
     // array initialization:
     for (int i = 0; i < n*n; i++){
-        hA[i] = static_cast <T> (rand() / static_cast <T> (RAND_MAX / 20)) - 10;
-        hB[i] = static_cast <T> (rand() / static_cast <T> (RAND_MAX / 20)) - 10;
+        hA[i] = static_cast <int> (rand() / static_cast <int> (RAND_MAX / 20)) - 10;
+        hB[i] = static_cast <int> (rand() / static_cast <int> (RAND_MAX / 20)) - 10;
     }
 
     auto start = chrono::steady_clock::now();
 
-    cudaMalloc((void**)&dA, sizeof(T) * n * n);
-    cudaMalloc((void**)&dB, sizeof(T) * n * n);
-    cudaMalloc((void**)&dC, sizeof(T) * n * n);
-    cudaMemcpy(dA, &hA, sizeof(T) * n * n, cudaMemcpyHostToDevice);
-    cudaMemcpy(dB, &hB, sizeof(T) * n * n, cudaMemcpyHostToDevice);
-    cudaMemset(dC, 0, n * n * sizeof(T));
+    cudaMalloc((void**)&dA, sizeof(int) * n * n);
+    cudaMalloc((void**)&dB, sizeof(int) * n * n);
+    cudaMalloc((void**)&dC, sizeof(int) * n * n);
+    cudaMemcpy(dA, &hA, sizeof(int) * n * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, &hB, sizeof(int) * n * n, cudaMemcpyHostToDevice);
+    cudaMemset(dC, 0, n * n * sizeof(int));
 
     // do math:
-    cout << "Switch time\n";
-    switch(type){
-        case 1:
-            cout << "Calculating type: int" << endl;
-            matmul_1(dA, dB, dC, n, blockSize);
-            break;
-        case 2:
-            cout << "Calculating type: float" << endl;
-            matmul_2(dA, dB, dC, n, blockSize);
-            break;
-        case 3:
-            cout << "Calculating type: double" << endl;
-            matmul_3(dA, dB, dC, n, blockSize);
-            break;
-        default:
-            cout << "Invalid type to process matmul.\n";
-            return;
-    }
+    cout << "Calculating type: int" << endl;
+    matmul_1(dA, dB, dC, n, blockSize);
 
     // results:
-    cudaMemcpy(&hC, dC, sizeof(T) * n * n, cudaMemcpyDeviceToHost);
+    cudaMemcpy(&hC, dC, sizeof(int) * n * n, cudaMemcpyDeviceToHost);
+
+    auto end = chrono::steady_clock::now();
+	auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    cout << "element count:  \t" << n << endl;
+	cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
+	cout << "first element:  \t" << hC[0] << endl;
+	cout << "last element:   \t" << hC[n*n - 1] << endl << endl;
+
+    cudaFree(dB);
+    cudaFree(dA);
+    cudaFree(dC);
+}
+
+void doFloat(int n, int blockSize){
+    float hB[n*n], hA[n*n], hC[n*n], *dB, *dA, *dC;
+
+    // randomization:
+    srand(chrono::system_clock::now().time_since_epoch().count());
+
+    // array initialization:
+    for (int i = 0; i < n*n; i++){
+        hA[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 20)) - 10;
+        hB[i] = static_cast <float> (rand() / static_cast <float> (RAND_MAX / 20)) - 10;
+    }
+
+    auto start = chrono::steady_clock::now();
+
+    cudaMalloc((void**)&dA, sizeof(float) * n * n);
+    cudaMalloc((void**)&dB, sizeof(float) * n * n);
+    cudaMalloc((void**)&dC, sizeof(float) * n * n);
+    cudaMemcpy(dA, &hA, sizeof(float) * n * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, &hB, sizeof(float) * n * n, cudaMemcpyHostToDevice);
+    cudaMemset(dC, 0, n * n * sizeof(float));
+
+    // do math:
+    cout << "Calculating type: float" << endl;
+    matmul_1(dA, dB, dC, n, blockSize);
+
+    // results:
+    cudaMemcpy(&hC, dC, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
+
+    auto end = chrono::steady_clock::now();
+	auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    cout << "element count:  \t" << n << endl;
+	cout << "time to process:\t" << (timePassed.count() / 1000) << " milliseconds\n";
+	cout << "first element:  \t" << hC[0] << endl;
+	cout << "last element:   \t" << hC[n*n - 1] << endl << endl;
+
+    cudaFree(dB);
+    cudaFree(dA);
+    cudaFree(dC);
+}
+
+void doDouble(int n, int blockSize){
+    double hB[n*n], hA[n*n], hC[n*n], *dB, *dA, *dC;
+
+    // randomization:
+    srand(chrono::system_clock::now().time_since_epoch().count());
+
+    // array initialization:
+    for (int i = 0; i < n*n; i++){
+        hA[i] = static_cast <double> (rand() / static_cast <double> (RAND_MAX / 20)) - 10;
+        hB[i] = static_cast <double> (rand() / static_cast <double> (RAND_MAX / 20)) - 10;
+    }
+
+    auto start = chrono::steady_clock::now();
+
+    cudaMalloc((void**)&dA, sizeof(double) * n * n);
+    cudaMalloc((void**)&dB, sizeof(double) * n * n);
+    cudaMalloc((void**)&dC, sizeof(double) * n * n);
+    cudaMemcpy(dA, &hA, sizeof(double) * n * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, &hB, sizeof(double) * n * n, cudaMemcpyHostToDevice);
+    cudaMemset(dC, 0, n * n * sizeof(double));
+
+    // do math:
+    cout << "Calculating type: double" << endl;
+    matmul_1(dA, dB, dC, n, blockSize);
+
+    // results:
+    cudaMemcpy(&hC, dC, sizeof(double) * n * n, cudaMemcpyDeviceToHost);
 
     auto end = chrono::steady_clock::now();
 	auto timePassed = chrono::duration_cast<std::chrono::microseconds>(end - start);
@@ -70,13 +135,13 @@ int main(int argc, char* argv[]) {
     cout << "I should execute matmul_" << type << " with an " << n << "x" << n << " matrix and " << block << " blocks\n";
     switch(type){
         case 1: 
-            doMatmul<int>(n, block, type);
+            doInt(n, block);
             break;
         case 2:
-            doMatmul<float>(n, block, type);
+            doFloat(n, block);
             break;
         case 3:
-            doMatmul<double>(n, block, type);
+            doDouble(n, block);
             break;
     }
 
