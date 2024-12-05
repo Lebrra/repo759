9073e1f45c34b6ac7eae0ff34f00b1ddd28ec7ac
Leah <scout1665@gmail.com>
2024-12-05 09:31:29 -0600

    fixed sh

--- HW07/task1.sh
+++ HW07/task1.sh
@@ -9,7 +9,7 @@
 
 module load nvidia/cuda/11.8.0
 module load gcc/11.3.0
-nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -stdc++17 -o task1
+nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
 date
 
 ./task1 512 512;
